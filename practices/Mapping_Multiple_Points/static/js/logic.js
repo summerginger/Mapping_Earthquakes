a67d778bd85178c0791d86a94cbb0947fbd59d79
summerginger <janehcanada@gmail.com>
2021-07-25 17:49:21 -0400

    allfiles

--- practices/Mapping_Multiple_Points/static/js/logic.js
+++ practices/Mapping_Multiple_Points/static/js/logic.js
@@ -9,47 +9,25 @@ console.log("working");
 // The setView() method sets the view of the map with a geographical center, where the first coordinate is latitude (40.7)
 //  and the second is longitude (-94.5). We set the zoom level of "4" on a scale 0–18.
 
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+  let map = L.map('mapid').setView([40.7, -94.5], 4);
+
 // Get data from cities.js
-// Coordinates for each point to be used in the line.
-// Coordinates for each point to be used in the polyline.
-let line = [
-  // SFO
-  [37.6213,-122.3790], 
-   // Austin-Bergstrom International Airport coordinates
-   [30.1975, -97.6664],
-   // YYZ
-  [43.6777, -79.6248],
-  // JFK
-  [40.6413, -73.7781]
-  
-];
-
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-  color: "blue",
-  weight: '3', 
-  dashArray: '0.5, 0.5'
-}).addTo(map);
- 
-let cityData = cities;
-
+  let cityData = cities ;
 // Loop through the cities array and create one marker for each city.
   cityData.forEach(function(city) {
-    console.log(city)
-    // L.marker(city.location)skill drill 13.4.2
-    L.circleMarker(city.location, {
-      color:'orange',
-      lineweight:4,
-      radius: city.population/200000
-      })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-  
-  });
+  console.log(city)
+  // L.marker(city.location)skill drill 13.4.2
+  L.circleMarker(city.location, {
+    color:'orange',
+    lineweight:4,
+    radius: city.population/200000
+    })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+.addTo(map);
+});
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     // id: 'mapbox/streets-v11',
@@ -59,4 +37,3 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
-
